f1b86900899058d57e947ef5b3fe54b4649bb3bd
Merk68 <marko.mark2001@gmail.com>
2021-09-17 08:06:24 +0200

    A feladat "csontvázának" létrehozása

--- Markó Márk/Program.cs
+++ Markó Márk/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Markó_Márk
 {
@@ -10,6 +11,48 @@ namespace Markó_Márk
   {
     static void Main(string[] args)
     {
+      ElsőFeladat();
+      MásodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      HetedikFeladat();
+    }
+
+    private static void HetedikFeladat()
+    {
+     
+    }
+
+    private static void HatodikFeladat()
+    {
+      
+    }
+
+    private static void OtodikFeladat()
+    {
+      
+    }
+
+    private static void NegyedikFeladat()
+    {
+      
+    }
+
+    private static void HarmadikFeladat()
+    {
+      
+    }
+
+    private static void MásodikFeladat()
+    {
+      
+    }
+
+    private static void ElsőFeladat()
+    {
+      
     }
   }
 }
